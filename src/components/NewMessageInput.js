27bb950c2c56fadd0ee1dddb2596dbbbe2d9196a
Filip Wiśniewski <fwfw541111@gmail.com>
2023-04-22 15:19:44 +0200

    Dummy image input on NewMessageInput

--- src/components/NewMessageInput.js
+++ src/components/NewMessageInput.js
@@ -17,6 +17,11 @@ const NewMessageInput = () => {
 
     return (
         <form className="newMessageInput" onSubmit={handlePost}>
+            <label htmlFor="message_img" className="newMessageInput__fileInputLabel">
+                <img src="/images/image_icon.png" alt="Send" className="newMessageInput__fileInputLabel__img"/>
+            </label>
+            <input id="message_img" name="message_img" type="file" className="newMessageInput__fileInput"
+                   accept=".jpg, .jpeg, .png, .gif"/>
             <input type="text" value={content} className="newMessageInput__input" onInput={e => setContent(e.target.value) }/>
             <input type="image" className="newMessageInput__btn" src="/images/send.png" alt="Send"/>
         </form>
