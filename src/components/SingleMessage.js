a972264f1c65249af734776d4b252a40f2507f4d
Filip Wiśniewski <fwfw541111@gmail.com>
2023-04-23 23:34:23 +0200

    Upload and download images from aws s3

--- src/components/SingleMessage.js
+++ src/components/SingleMessage.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const SingleMessage = React.forwardRef(({message, modifier, dateFormatter}, ref) => {
+    const [imageData, setImageData] = useState('')
+
+    const fetchImage = async () =>
+        await axios.get(`http://localhost:8080/message/file/${message.content}`, {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+
+    useEffect(() => {
+        if (message.type === 'FILE') {
+            fetchImage()
+                .then(res => setImageData(`data:image/jpeg;base64,${res.data}`))
+        }
+    }, [])
+
     return (
         <>
             {!dateFormatter.is5MinDiffBefore() &&
@@ -15,7 +32,7 @@ const SingleMessage = React.forwardRef(({message, modifier, dateFormatter}, ref)
                     </div>
                     :
                     <div ref={ref} id={`messageId${message.id}`} className={`messages__singleMessage${modifier} messages__singleMessage--img`}>
-                        <img src={`http://localhost:8080${message.content}`} alt="" className="messages__singleMessage__img"/>
+                        <img src={imageData} alt="" className="messages__singleMessage__img"/>
                     </div>
             }
         </>
